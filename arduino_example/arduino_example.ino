f5f0e9f7efb0fbcf4c83a0ecf13d432558bb9a7c
Filipe Cruz <psenough@gmail.com>
2018-12-08 23:30:16 +0000

    debouncer & remove timer dependency

--- arduino_example/arduino_example.ino
+++ arduino_example/arduino_example.ino
@@ -1,12 +1,20 @@
-#include <Event.h>
-#include <Timer.h>
+//#include <Event.h>
+//#include <Timer.h>
 
 // https://github.com/JChristensen/Timer/archive/v2.1.zip
-Timer t;
+//Timer t;
 
 // digital pin 2 has a pushbutton attached to it. Give it a name:
 int pushButton = 2;
 int ledPin = 13;
+
+unsigned long lastDebounceTime;
+unsigned long debounceTime = 10;
+int prevButtonState = 0;
+
+unsigned long lastBlink;
+unsigned long blinkRate = 1000;
+bool blinkState;
 
 // the setup routine runs once when you press reset:
 void setup() {
@@ -15,20 +23,46 @@ void setup() {
   // make the pushbutton's pin an input:
   //pinMode(pushButton, INPUT);
   pinMode(pushButton, INPUT_PULLUP);
-  
+
   pinMode(ledPin, OUTPUT);
-  digitalWrite(ledPin, HIGH);
-  
-  t.oscillate(ledPin, 500, LOW);
+  digitalWrite(ledPin, LOW);
+
+  //t.oscillate(ledPin, 500, LOW);
   //t.every(1000, takeReading);
+
+  lastDebounceTime = 0;
+  lastBlink = 0;
+  blinkState = true;
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
+  unsigned long curr = millis();
+  
   // read the input pin:
   int buttonState = digitalRead(pushButton);
-  // print out the state of the button:
-  Serial.println(buttonState);
-  delay(1);        // delay in between reads for stability
-  t.update();
+
+  if (curr > lastDebounceTime) {
+    if (buttonState != prevButtonState) {
+      Serial.println(buttonState);
+      prevButtonState = buttonState;
+    }
+    lastDebounceTime = curr;
+  }
+  //Serial.println(buttonState);
+ 
+  if (curr > lastBlink + blinkRate) {
+    if (blinkState == true) {
+      digitalWrite(ledPin, LOW);
+      blinkState = false;
+      //Serial.println("off");
+    } else {
+      digitalWrite(ledPin, HIGH);
+      blinkState = true;
+      //Serial.println("on");
+    }
+    lastBlink = curr;
+  }
+  
+  delay(1); // delay in between reads for stability
 }
